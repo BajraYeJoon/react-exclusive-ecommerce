c97cb5b0d47c9d943b3d12e570073d05b530a0c6
bajrayejoon <binaya.bajrashakya@gmail.com>
2024-07-16 16:15:00 +0545

    chore: Remove commented out code in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-
-
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/route";
 
@@ -8,17 +6,7 @@ function App() {
     <>
       <RouterProvider router={router} />
     </>
-    // <main className="mx-auto">
-    //   <Banner />
-    //   <Navbar />
-    //   <div className="flex flex-col gap-40 max-3xl:gap-32 max-2xl:gap-28 mb-28 mx-72 max-3xl:mx-24 max-2xl:mx-14 ">
-    //     <Hero />
-    //     <SalesCard />
-    //     <Category />
-    //     <BestProducts />
-    //   </div>
-    // </main>
   );
 }
 
-export default App
+export default App;
