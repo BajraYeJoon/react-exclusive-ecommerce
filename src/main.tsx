fb2bef0e23b93c5242d625bc560e28ffec9f25d1
bajrayejoon <binaya.bajrashakya@gmail.com>
2024-07-18 08:01:05 +0545

    feat: Update package dependencies and add posthog-js library

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
+import { PostHogProvider } from "posthog-js/react";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
+
+const options = {
+  api_host: "https://us.i.posthog.com",
+};
 
 const query = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={query}>
-      <App />
+      <PostHogProvider
+        apiKey="phc_fQcn9SW8lEM635UzO6NIJQwS4OPo2tsakZJriWF44Nx"
+        options={options}
+      >
+        <App />
+      </PostHogProvider>
     </QueryClientProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
