4de2fc774065af4a32a4b785f07f86ccdf934bf6
bajrayejoon <binaya.bajrashakya@gmail.com>
2024-10-03 09:32:05 +0545

    update: Home component to add offer images gif

--- src/user-portal/site/home/Home.tsx
+++ src/user-portal/site/home/Home.tsx
@@ -10,20 +10,36 @@ import {
 } from "../../pages";
 import { Spotlight } from "../../../common/components";
 
+const offer_gif =
+  "https://res.cloudinary.com/dw55twddi/image/upload/v1727926551/exclusive/public/khgif_ebuksr.gif";
+
+const offer_img =
+  "https://res.cloudinary.com/dw55twddi/image/upload/v1727926596/exclusive/public/offer_jmqgqo.jpg";
+
 const Home = () => {
-	return (
-		<div className="relative mx-72 flex flex-col gap-16 overflow-x-hidden max-2xl:mx-6 max-2xl:gap-20">
-			<Hero />
-			<SalesCard />
-			<Category />
-			<Spotlight />
-			<BestProducts />
-			<LimitedEditionCTA />
-			<GeneralProducts />
-			<ArrivalProductsGrid />
-			<ServiceDetails />
-		</div>
-	);
+  return (
+    <div className="relative mx-72 flex flex-col gap-16 overflow-x-hidden max-2xl:mx-6 max-2xl:gap-14">
+      <Hero />
+      <SalesCard />
+      <img
+        src={offer_gif}
+        alt="Offer_img"
+        className="aspect-auto h-24 object-cover object-left lg:h-auto lg:w-auto"
+      />
+      <Category />
+      <Spotlight />
+      <BestProducts />
+      <LimitedEditionCTA />
+      <GeneralProducts />
+      <img
+        src={offer_img}
+        alt="Offer_img"
+        className="aspect-auto h-24 object-cover object-left lg:h-auto lg:w-auto"
+      />
+      <ArrivalProductsGrid />
+      <ServiceDetails />
+    </div>
+  );
 };
 
 export { Home };
